19a76957ade10d5f382b46431cd9ac4f74998a90
Laurent Destailleur <eldy@users.sourceforge.net>
2010-01-13 16:26:23 +0000

    Prepare module numberwords

--- numberwords/htdocs/includes/modules/substitutions/functions_numberwords.lib.php
+++ numberwords/htdocs/includes/modules/substitutions/functions_numberwords.lib.php
@@ -21,12 +21,12 @@
  *	\file			htdocs/lib/functionsnumberswords.lib.php
  *	\brief			A set of functions for Dolibarr
  *					This file contains functions for plugin numberwords.
- *	\version		$Id: functions_numberwords.lib.php,v 1.3 2009/11/06 21:40:44 eldy Exp $
+ *	\version		$Id: functions_numberwords.lib.php,v 1.4 2010/01/13 16:26:23 eldy Exp $
  */
 
 
 /**
- * 		\brief		Function called to complete subsitution array
+ * 		\brief		Function called to complete substitution array
  * 					functions xxx_completesubstitutionarray are called by make_substitutions()
  * 		\return		The entry parameter $substitutionarray is modified
  */
@@ -35,18 +35,21 @@ function numberwords_completesubstitutionarray(&$substitutionarray,$langs,$objec
 	global $conf;
 	if (is_object($object))
 	{
-		$numbertext=$langs->getLabelFromNumber($object->total_ttc,1,$conf->monnaie);
+		$numbertext=$langs->getLabelFromNumber($object->total_ttc,1);
 		$substitutionarray['__TOTAL_TTC_WORDS__']=$numbertext;
-		$numbertext=$langs->getLabelFromNumber($object->total_ht,1,$conf->monnaie);
+		$numbertext=$langs->getLabelFromNumber($object->total_ht);
 		$substitutionarray['__TOTAL_HT_WORDS__']=$numbertext;
-		$numbertext=$langs->getLabelFromNumber($object->total_vat,1,$conf->monnaie);
+		$numbertext=$langs->getLabelFromNumber($object->total_vat,1);
 		$substitutionarray['__TOTAL_VAT_WORDS__']=$numbertext;
+		$numbertext=$langs->getLabelFromNumber($object->number,0);
+		$substitutionarray['__NUMBER_WORDS__']=$numbertext;
 	}
 }
 
 /**
- *      \brief      Return full text translated to languagea label for a key. Store key-label in a cache.
- *		\number		number		Number to encode in full text
+ *      \brief      Return full text translated to language label for a key. Store key-label in a cache.
+ *		\param		langs		Language for output
+ * 		\param		number		Number to encode in full text
  * 		\param		isamount	1=It's an amount, 0=it's just a number
  *      \return     string		Label translated in UTF8 (but without entities)
  * 								10 if setDefaultLang was en_US => ten
@@ -58,7 +61,7 @@ function numberwords_getLabelFromNumber($langs,$number,$isamount=0)
 
 	dol_syslog("numberwords_getLabelFromNumber langs->defaultlang=".$langs->defaultlang." number=".$number." isamount=".$isamount);
 	$outlang=$langs->defaultlang;	// Output language we want
-	$outlangarray=split('_',$outlang,2);
+	$outlangarray=explode('_',$outlang,2);
 	// If lang is xx_XX, then we use xx
 	if (strtolower($outlangarray[0]) == strtolower($outlangarray[1])) $outlang=$outlangarray[0];
 
@@ -78,16 +81,31 @@ function numberwords_getLabelFromNumber($langs,$number,$isamount=0)
 
 	if (! file_exists($handle->dir.'Numbers/Words/lang.'.$outlang.'.php'))
 	{
-		return "(Error: No rule file to convert number to text for language ".$langs->defaultlang.")";
+		return "(Error: No rule file into Numbers/Words to convert number to text for language ".$langs->defaultlang.")";
 	}
 
-	// Overwrite label of currency and cents to ours
-	$handle->labelcurrency=$conf->monnaie;
-	$handle->labelcents='cent';
-	if ($langs->transnoentitiesnoconv("CurrencySing".$conf->monnaie)!='CurrencySing'.$conf->monnaie) $handle->labelcurrency=$langs->transnoentitiesnoconv("CurrencySing".$conf->monnaie);
-	elseif ($langs->transnoentitiesnoconv("Currency".$conf->monnaie)!='Currency'.$conf->monnaie) $handle->labelcurrency=$langs->transnoentitiesnoconv("Currency".$conf->monnaie);
-	if ($langs->transnoentitiesnoconv("CurrencyCent".$conf->monnaie)!='CurrencyCent'.$conf->monnaie) $handle->labelcents=$langs->transnoentitiesnoconv("CurrencyCent".$conf->monnaie);
+	// Define label on currency and cent in the property of object handle
+	$handle->labelcurrency=$conf->monnaie;	// By default
+	$handle->labelcents='cent';				// By default
 
+	// Overwrite label of currency to ours
+	$labelcurrencysing=$langs->transnoentitiesnoconv("CurrencySing".$conf->monnaie);
+	if ($labelcurrencysing && $labelcurrencysing != -1 && $labelcurrencysing!='CurrencySing'.$conf->monnaie) $handle->labelcurrency=$labelcurrencysing;
+	else
+	{
+		$labelcurrency=$langs->transnoentitiesnoconv("Currency".$conf->monnaie);
+		if ($labelcurrency && $labelcurrency !='Currency'.$conf->monnaie) $handle->labelcurrency=$labelcurrency;
+	}
+	// Overwrite label of cent to ours
+	$labelcurrencycentsing=$langs->transnoentitiesnoconv("CurrencyCentSing".$conf->monnaie);
+	if ($labelcurrencycentsing && $labelcurrencycentsing != -1 &&$labelcurrencycentsing!='CurrencyCentSing'.$conf->monnaie) $handle->labelcents=$labelcurrencycentsing;
+	else
+	{
+		$labelcurrencycent=$langs->transnoentitiesnoconv("CurrencyCent".$conf->monnaie);
+		if ($labelcurrencycent && $labelcurrencycent !='CurrencyCent'.$conf->monnaie) $handle->labelcents=$labelcurrencycent;
+	}
+
+	// Call method of object handle to make convertion
 	if ($isamount) $numberwords=$handle->toCurrency($number, $outlang, $conf->monnaie);
 	else $numberwords=$handle->toWords($number, $outlang);
 
