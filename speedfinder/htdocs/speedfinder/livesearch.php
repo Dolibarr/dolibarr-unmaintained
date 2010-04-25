53be122e7d6c35236265ab0c2b9eb61748927e49
Laurent Destailleur <eldy@users.sourceforge.net>
2010-04-25 14:34:46 +0000

    Using lower is useless a a database must no be case senitive

--- speedfinder/htdocs/speedfinder/livesearch.php
+++ speedfinder/htdocs/speedfinder/livesearch.php
@@ -8,7 +8,7 @@
     	\file       htdocs/speedfinder/livesearch.php
 		\ingroup    speedfinder
 		\brief      Speedfinder AJAX SQL result constructor page
-		\version    $Id: livesearch.php,v 1.2 2009/11/30 22:37:40 hregis Exp $
+		\version    $Id: livesearch.php,v 1.3 2010/04/25 14:34:46 eldy Exp $
 		\author		Marc STUDER
 */
 
@@ -33,7 +33,7 @@ if (strlen($q) >= $NBCAR_MINI) {
 	$q = strtolower($q);
 	$sql_search = 'SELECT SOC.rowid as SOCid, SOC.nom, SOC.tel, CTC.rowid as CTCid, CTC.name, CTC.firstname, CTC.phone, CTC.phone_perso, CTC.phone_mobile '.
 	    'FROM '.MAIN_DB_PREFIX.'societe SOC,'.MAIN_DB_PREFIX.'socpeople CTC '.
-	    'WHERE CTC.fk_soc=SOC.rowid AND (LOWER(SOC.nom) like "%'.$q.'%" OR LOWER(CTC.name) like "%'.$q.'%" OR SOC.tel like "%'.$q.'%" OR CTC.phone like "%'.$q.'%" OR CTC.phone_perso like "%'.$q.'%" OR CTC.phone_mobile like "%'.$q.'%")';
+	    'WHERE CTC.fk_soc=SOC.rowid AND (SOC.nom like "%'.$q.'%" OR CTC.name like "%'.$q.'%" OR SOC.tel like "%'.$q.'%" OR CTC.phone like "%'.$q.'%" OR CTC.phone_perso like "%'.$q.'%" OR CTC.phone_mobile like "%'.$q.'%")';
 
 
 //DEBUG
@@ -58,24 +58,24 @@ if (strlen($q) >= $NBCAR_MINI) {
 			//TODO : les separateurs de contact ne sont pas aussi bien gérés que les séparateurs de société
 			// soluce : saisir les num de contacts sans separateur !!!
 			$currPhone = "";
-			if ( !(strrpos($obj->phone, $q) === false) ) 
+			if ( !(strrpos($obj->phone, $q) === false) )
 				$currPhone .= $SEPAR_PHONE.$obj->phone;
-			if ( !(strrpos($obj->phone_perso, $q) === false) ) 
+			if ( !(strrpos($obj->phone_perso, $q) === false) )
 				$currPhone .= $SEPAR_PHONE.$obj->phone_perso;
-			if ( !(strrpos($obj->phone_mobile, $q) === false) ) 
+			if ( !(strrpos($obj->phone_mobile, $q) === false) )
 				$currPhone .= $SEPAR_PHONE.$obj->phone_mobile;
 			if ( !(strrpos(strtolower($obj->name), $q) === false) ) {
 				if ( !empty($obj->phone) ) $currPhone .= $SEPAR_PHONE.$obj->phone;
 				if ( !empty($obj->phone_perso) ) $currPhone .= $SEPAR_PHONE.$obj->phone_perso;
 				if ( !empty($obj->phone_mobile) ) $currPhone .= $SEPAR_PHONE.$obj->phone_mobile;
-				//si aucun tel pour ce contact, on force l'affichage du contact quand meme				
+				//si aucun tel pour ce contact, on force l'affichage du contact quand meme
 				if ( empty($currPhone) ) $currPhone .= ".";
 			}
 			if (!empty($currPhone)) {
 				$contactSoc = "";
 				if (empty($retraitContactassoc)) $contactSoc = $obj->nom." : ";
 				$hint=$hint.$retraitContactassoc."<a href='../contact/fiche.php?id=".$obj->CTCid."' target='_top'>".$contactSoc.$obj->name." ".$obj->firstname.$currPhone."</a><br />";
-			
+
 			}
 			$retraitContactassoc = "";
 			$i++;
