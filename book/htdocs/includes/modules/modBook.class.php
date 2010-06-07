e91065a30415be9d09808103a2f572f2e7e5ff94
pit <pit>
2010-06-07 14:50:25 +0000

    Mod : Replacing eregi_replace by preg_replace

--- book/htdocs/includes/modules/modBook.class.php
+++ book/htdocs/includes/modules/modBook.class.php
@@ -59,10 +59,10 @@ class modBook extends DolibarrModules
 	  
 		$this->family = "products";
 		// Module label (no space allowed), used if translation string 'ModuleXXXName' not found (where XXX is value of numeric property 'numero' of module)
-		$this->name = eregi_replace('/^mod/i','',get_class($this));
+		$this->name = preg_replace('/^mod/i','',get_class($this));
 		$this->description = "Gestion des livres";
 
-		$this->revision = explode(' ','$Revision: 1.4 $');
+		$this->revision = explode(' ','$Revision: 1.5 $');
 		$this->version = $this->revision[1];
 
 		// Key used in llx_const table to save module status enabled/disabled
