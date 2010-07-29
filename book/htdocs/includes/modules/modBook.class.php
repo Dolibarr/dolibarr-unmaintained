750833cf9e887dc3fd2972d3a7160ae9e5aeb857
Cyrille de Lambert <cyrille.delambert@auguria.net>
2010-07-29 16:37:45 +0000

    Problème Smarty

--- book/htdocs/includes/modules/modBook.class.php
+++ book/htdocs/includes/modules/modBook.class.php
@@ -7,6 +7,7 @@
  * Copyright (C) 2008 	   Samuel  Bouchet		<samuel.bouchet@auguria.net>
  * Copyright (C) 2008 	   Patrick Raguin  		<patrick.raguin@auguria.net>
  * Copyright (C) 2010 	   Pierre Morin			<pierre.morin@auguria.net>
+ * Copyright (C) 2010 	   Cyrille de Lambert			<cyrille.delambert@auguria.net>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -62,12 +63,12 @@ class modBook extends DolibarrModules
 		$this->name = preg_replace('/^mod/i','',get_class($this));
 		$this->description = "Gestion des livres";
 
-		$this->revision = explode(' ','$Revision: 1.5 $');
+		$this->revision = explode(' ','$Revision: 1.6 $');
 		$this->version = $this->revision[1];
 
 		// Key used in llx_const table to save module status enabled/disabled
 		$this->const_name = 'MAIN_MODULE_'.strtoupper($this->name);
-		$this->special = 0;
+		$this->special = 3;
 		$this->picto='product';
 
 		// Dependances
@@ -95,7 +96,9 @@ class modBook extends DolibarrModules
 		// List of parameters
 		$this->const = array(0=>array('BOOK_CONTRACT_BILL_SLOGAN','chaine','My society slogan','This is the slogan of the current Society, to be printed in contract bill mails.',0),
 							1=>array('BOOK_CONTRACT_BILL_SOCIETE_INFOS','chaine','TEL. 01 02 03 04 05, FAX 05 06 07 08 09 - SARL AU CAPITAL DE 7 500 EUROS - SIRET - 419 700 425 88865','','This is the informations about the current Society, to be printed in contract bill mails.',0),
-							2=>array('BOOK_CONTRACT_BILL_OTHER_INFOS','chaine','www.mysociety.com','This is other informations about the current Society, to be printed in contract bill mails.',0)/*,
+							2=>array('BOOK_CONTRACT_BILL_OTHER_INFOS','chaine','www.mysociety.com','This is other informations about the current Society, to be printed in contract bill mails.',0),
+							3=>array('MAIN_MODULE_BOOK_NEEDSMARTY','chaine',1,'Need smarty',0)
+							/*,
 							3=>array('BOOK_SUBPERMCATEGORY_FOR_DOCUMENTS','chaine','book','This is the name of the sub-permissions to use to get the documents for this module',0),
 							4=>array('BOOK_SQLPROTECTAGAINSTEXTERNALS_FOR_DOCUMENTS','chaine','SELECT rowid as fk_soc FROM ".MAIN_DB_PREFIX."societe WHERE rowid=\'$refname\'','This is the SQL command to execute for the basic protection against external users',0)*/);
 		//Start of user code - Constants are not generated, must be implemented. The code within user code markups while NOT be lost if the file is generated again
